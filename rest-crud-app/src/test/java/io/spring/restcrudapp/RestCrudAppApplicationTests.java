9fde693d965e7c2f71d7f361cdca8fe0e640b969
Anthony Dahanne <anthony.dahanne@gmail.com>
2023-02-17 15:53:40 -0500

    Initial commit

---
+++ rest-crud-app/src/test/java/io/spring/restcrudapp/RestCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.restcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
